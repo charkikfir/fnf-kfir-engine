720ac3c575650a3880ab01066b0285bf559af625
charkikfir <64697091+charkikfir@users.noreply.github.com>
2022-10-18 17:20:54 +0300

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,11 +1,10 @@
 package;
 
 #if android
+import android.Hardware;
 import android.Permissions;
 import android.os.Build.VERSION;
 import android.os.Environment;
-import android.Hardware;
-import android.Hardware.toast;
 #end
 import flash.system.System;
 import flixel.FlxG;
