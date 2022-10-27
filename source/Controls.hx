f5bfa911c77b949ecc0d291980f3dc9c4b79eb2d
charkikfir <64697091+charkikfir@users.noreply.github.com>
2022-10-27 15:37:26 +0300

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -15,6 +15,11 @@ import flixel.input.actions.FlxActionSet;
 import flixel.input.gamepad.FlxGamepadButton;
 import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.keyboard.FlxKey;
+#if mobile
+import mobile.flixel.FlxButton;
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.FlxVirtualPad;
+#end
 
 #if (haxe >= "4.0.0")
 enum abstract Action(String) to String from String
