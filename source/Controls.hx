3f05c10171adeaa825d49c00803328e5a70f0128
charkikfir <64697091+charkikfir@users.noreply.github.com>
2022-10-31 10:22:28 +0200

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -384,7 +384,7 @@ class Controls extends FlxActionSet
 	#end
 
 	
-	#if mobile
+		#if mobile
 	public var trackedinputsUI:Array<FlxActionInput> = [];
 	public var trackedinputsNOTES:Array<FlxActionInput> = [];
 
@@ -738,6 +738,26 @@ class Controls extends FlxActionSet
 		#else
 		forEachBound(control, function(action, _) removeKeys(action, copyKeys));
 		#end
+	}
+	#else
+	public function bindKeys(control:Control, keys:Array<FlxKey>)
+	{
+		#if (haxe >= "4.0.0")
+		inline forEachBound(control, (action, state) -> addKeys(action, keys, state));
+		#else
+		forEachBound(control, function(action, state) addKeys(action, keys, state));
+		#end
+	}
+
+	public function unbindKeys(control:Control, keys:Array<FlxKey>)
+	{
+		#if (haxe >= "4.0.0")
+		inline forEachBound(control, (action, _) -> removeKeys(action, keys));
+		#else
+		forEachBound(control, function(action, _) removeKeys(action, keys));
+		#end
+	}	
+	#end	
 	}
 	#else
 	public function bindKeys(control:Control, keys:Array<FlxKey>)
