824b9cb216ca3b7458e47f46ff10d626cca3600e
charkikfir <64697091+charkikfir@users.noreply.github.com>
2022-11-01 15:00:23 +0200

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -27,7 +27,7 @@ using StringTools;
 
 class Paths
 {
-	inline public static var SOUND_EXT = #if web "mp3" #else "ogg","mp3", "mid", "wav" #end;
+	inline public static var SOUND_EXT = #if web "mp3" #else "ogg","mp3", "mid", "wav"; #end;
 	inline public static var VIDEO_EXT = "mp4";
 
 	#if MODS_ALLOWED
